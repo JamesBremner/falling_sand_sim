6cd168c34a3a4e1ed28692dd45cef62ae3a9c5f5
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-24 21:40:50 +0300

    Update Constants.hpp

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
-#include <array>
-
 #include <SFML/Graphics.hpp>
 
 namespace Window {
     constexpr int Width = 2560;
     constexpr int Height = 1440;
     constexpr int Fps = 120;
+}
+
+
+namespace CellStats {
+    constexpr int Cell = 20;
+    constexpr sf::Vector2f Cell2f = {Cell, Cell};
+    constexpr sf::Color Color = sf::Color(194, 178, 128);
+}
+
+namespace GridP {
+    constexpr int x_cells = Window::Width / CellStats::Cell;
+    constexpr int y_cells = Window::Height / CellStats::Cell;
 }
