b61c004fe4de0f0897102dec9d3fb866d52e896a
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-24 22:16:22 +0300

    Set methods added

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -11,7 +11,22 @@ Grid::Grid() : grid_(GridP::x_cells, GridP::y_cells) {
 void Grid::draw(sf::RenderWindow& win) {
     for (int y = 0; y < grid_.rows(); ++y) {
         for (int x = 0; x < grid_.cols(); ++x) {
-            win.draw(grid_[y][x].get());
+            if (!grid_[y][x].is_free()) {
+                win.draw(grid_[y][x].get());
+            }
+        }
+    }
+}
+
+void Grid::set_is_free(sf::Vector2i mouse_pos) {
+    for (int y = 0; y < grid_.rows(); ++y) {
+        for (int x = 0; x < grid_.cols(); ++x) {
+            if (mouse_pos.x >= x * CellStats::Cell && mouse_pos.x <= x * CellStats::Cell + CellStats::Cell &&  // check left side and right side
+                mouse_pos.y >= y * CellStats::Cell && mouse_pos.y <= y * CellStats::Cell + CellStats::Cell &&  // check top and bottom
+                grid_[y][x].is_free()) { // Check the availability of the cell
+                    grid_[y][x].set_is_free(false);
+                    std::cout << "Created\n";
+                }
         }
     }
 }
